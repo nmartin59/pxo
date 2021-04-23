26cc4acaab4c7e12057fb4f4d9c14d76b027efe8
nathan.martin <nathan.martin@praxedo.com>
2021-04-23 17:40:52 +0200

    on est bon

---
+++ _preview.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="_preview" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="../Users/nmartin/Downloads/LPC Women&amp;#039;s Shirt Pack/Sleeveless Shirt/_preview.png" width="64" height="64"/>
+</tileset>
